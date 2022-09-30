f0c1f94a059556c0b20731d9ccca86f1ae1b9683
gauripatole <patole.g@northeastern.edu>
2022-09-30 01:09:51 -0400

    Adding create profile.

---
+++ Employer/src/EmployeeProfile.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gauri
+ */
+public class EmployeeProfile {
+   
+    private String Name;
+    private int EmployeeId;
+    private int age;
+    private String gender;
+    private String StartDate;
+    private String level;
+    private String TeamInfo;
+    private int PhoneNo;
+    private String email;    
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(int EmployeeId) {
+        this.EmployeeId = EmployeeId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public int getPhoneNo() {
+        return PhoneNo;
+    }
+
+    public void setPhoneNo(int PhoneNo) {
+        this.PhoneNo = PhoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
